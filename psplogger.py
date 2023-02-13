56e202978c5e00487ecc11e2fc9b186449ddd594
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-02-13 16:08:05 -0600

    Agregar clase para el flujo principal.

--- psplogger.py
+++ psplogger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import time
+import sys
 
 FILENAME = "log.txt"
 
@@ -12,7 +13,7 @@ MSG_STOP = "Stopped activity ({}): {} min total delta, {} min total interrupted.
 class Interruption:
     def __init__(self):
         self.__start_date = time.time()
-        self.__end_date = None
+        self.__end_date = 0.0
         self.__active = True
     
     def get_start_date(self):
@@ -110,4 +111,96 @@ class ActivityLog:
                 activity.get_delta_time(),
                 comment))
 
-# TODO: implement main sequence
+class Application:
+    def __init__(self):
+        self.__program_name = ''
+        self.__phase_name = ''
+        self.__error_msg = False
+        self.__info_msg = False
+        self.__options = {'s':'(s)tart', 'i':'(i)nterrupt', 'r':'(r)esume', 't':'s(t)op', 'q': '(q)uit'}
+
+    def run(self):
+        activity = PspActivity()
+        while True:
+            status = activity.get_status()
+            valid_options = []
+            if status == 'stopped':
+                self.__save(activity)
+                break
+            elif status == 'not_started':
+                valid_options.extend(['s', 'q'])
+            elif status == 'active':
+                valid_options.extend(['i', 't'])
+            elif status == 'interrupted':
+                valid_options.extend(['r', 't']) 
+            prompt = self.__gen_prompt(status, self.__info_msg, self.__error_msg, valid_options)
+            command = input(prompt)
+            self.__error_msg = False # limpiar el error msg luego de generar el prompt
+            self.__info_msg = False # limpiar el info msg luego de generar el prompt
+            if command in valid_options:
+                self.__info_msg = self.__execute_command(command, activity)
+            else:
+                self.__error_msg = 'Invalid command.'
+                continue
+
+    def __save(self, activity: PspActivity):
+        comment = input('[?] Write a short comment: ')
+        ActivityLog(self.__program_name, self.__phase_name, activity, comment)
+
+    def __execute_command(self, command: str, activity: PspActivity) -> str:
+        info = ''
+        if command == 's':
+            info = activity.start()
+        elif command == 'i':
+            info = activity.interrupt()
+        elif command == 'r':
+            info = activity.resume()
+        elif command == 't':
+            info = activity.stop()
+        elif command == 'q':
+            exit(0)
+        return info
+
+    def __gen_prompt(self, activity_status, info, error, valid_options: list) -> str:
+        msg = '\n[ {} ]'.format(activity_status)
+        if info != False:
+            msg += ' INFO: {}'.format(info)
+        if error != False:
+            if info != False:
+                msg += ' |'
+            msg += ' ERROR: {}'.format(error)
+        msg += '\n{}'.format(self.__gen_command_str(valid_options))
+        msg += ' >> '
+        return msg
+
+    def __gen_command_str(self, options: list) -> str:
+        commands_str = ''
+        for o in options:
+            commands_str += self.__options[o]
+            if options[-1] != o:
+                commands_str += ' | '
+        return commands_str
+
+
+    def valid_params(self, params) -> bool:
+        if len(params) == 3:
+            self.__program_name = params[1]
+            self.__phase_name = params[2]
+            return True
+        else:
+            self.__error_msg = '[ ERROR: Invalid parameters. ]'
+            return False
+
+    def get_error(self):
+        return self.__error_msg
+
+    def get_help(self):
+        return 'USAGE:\n\tpython psplogger.py [PROGRAM_NAME] [PHASE_NAME]\nEXAMPLE:\n\tpython psplogger.py PSP0_1 Planning.Step1\n'
+
+# Main Sequence
+app = Application()
+if app.valid_params(sys.argv):
+    app.run()
+else:
+    print(app.get_error())
+    print(app.get_help())
