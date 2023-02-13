072a937ca141dae641f47dd38a8d84305c96a907
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-02-13 17:39:02 -0600

    FIX: Quitar la opción de parar cuando se está en una interrupción para evitar errores en el calculo de tiempo delta.

--- psplogger.py
+++ psplogger.py
@@ -133,7 +133,7 @@ class Application:
             elif status == 'active':
                 valid_options.extend(['i', 't'])
             elif status == 'interrupted':
-                valid_options.extend(['r', 't']) 
+                valid_options.extend(['r']) 
             prompt = self.__gen_prompt(status, self.__info_msg, self.__error_msg, valid_options)
             command = input(prompt)
             self.__error_msg = False # limpiar el error msg luego de generar el prompt
